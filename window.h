38113b013643817276f21fb69f5d56b7648004d7
VetaMaximova <54678796+VetaMaximova@users.noreply.github.com>
2020-11-23 05:01:18 +0300

    window function was added

---
+++ window.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   window.h
+ * Author: v.maksimova
+ *
+ * Created on 8 Апрель 2016 г., 21:05
+ */
+
+#ifndef WINDOW_H
+#define	WINDOW_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+ void dolphcheb(double *d, int n);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WINDOW_H */
+
